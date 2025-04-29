27016f52d3abc1c718e80f68dfb0223a77880c38
Charlie Prior <charlie@charlieprior.com>
2025-04-29 15:36:55 -0400

    Expose top-level classes and functions in __init__s

--- src/cohfit/__init__.py
+++ src/cohfit/__init__.py
@@ -1,3 +1,6 @@
 # SPDX-FileCopyrightText: 2025-present Charlie Prior <charlie@charlieprior.com>
 #
 # SPDX-License-Identifier: MIT
+
+from .classes.Ensemble import Ensemble
+from .classes.Experiment import Experiment
---
+++ src/cohfit/minimization/__init__.py
@@ -0,0 +1,1 @@
+from .feldmancousins import feldmancousins
